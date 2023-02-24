89d02c8864ce63eda5620d9ab524b103821e362c
Anastasia <anastasia2804@gmail.com>
2023-02-24 15:59:48 -0500

    completed

--- src/main/java/tech/strategio/E01.java
+++ src/main/java/tech/strategio/E01.java
@@ -4,7 +4,23 @@ import java.util.*;
 
 public class E01 {
     public int getNthFib(int n) {
-        // FIXME
-        return -1;
+        if (n == 1) {
+            return 0;
+        } else if (n == 2) {
+            return 1;
+        }
+
+        int prev1 = 0;
+        int prev2 = 1;
+        int current = 0;
+
+        for (int i = 3; i <= n; i++) {
+            current = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = current;
+        }
+
+        return current;
     }
 }
+
